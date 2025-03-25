d1a0d207c015edd44088ba130236f332183c82ba
Sofia Gomez <sofiafgm@gmail.com>
2025-03-25 18:58:35 +0100

    Test widgets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,38 +9,59 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
+    return MaterialApp(
+        title: 'Movie Finder',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 126, 60, 164)),
         ),
         home: MyHomePage(),
-      ),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
-
 class MyHomePage extends StatelessWidget {
-  @override
+  
+@override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    return Container(
+      color: const Color.fromARGB(255, 247, 248, 249),
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            "Flutter Widgets",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, decoration: TextDecoration.none),
+          ),
+          const SizedBox(height: 20), 
 
-    return Scaffold(
-      body: Center( 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome to Flutter'),
-            Text('“Hello World”'),
-          ],
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text("Row 1", style: TextStyle(color: Color.fromARGB(255, 2, 2, 2), fontSize: 18, decoration: TextDecoration.none)),
+              Text("Row 2", style: TextStyle(color: Color.fromARGB(255, 2, 2, 2), fontSize: 18, decoration: TextDecoration.none)),
+            ],
+          ),
+          const SizedBox(height: 20),
+      
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                color: Colors.blue,
+                child: const Center(
+                  child: Text(
+                    "Stack",
+                    style: TextStyle(color: Colors.white, fontSize: 20, decoration: TextDecoration.none),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
